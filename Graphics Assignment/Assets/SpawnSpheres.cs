c9fe8da92277d496ee937de350af64d0d1269d4a
AIE\s171699 <adrianpeds15@gmail.com>
2018-06-18 16:22:18 +1000

    First Commit

---
+++ Graphics Assignment/Assets/SpawnSpheres.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSpheres : MonoBehaviour {
+
+    public GameObject SpherePrefab;
+    public Transform SpawnPoint;
+
+    private void Start()
+    {
+    
+    }
+
+    public void spawnObject()
+    {
+        Instantiate(SpherePrefab, SpawnPoint.position + Random.insideUnitSphere * 10, Quaternion.identity);
+    }
+
+}
